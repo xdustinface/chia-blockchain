ac133e5f875f7b1e23e37586c4720aba3418eac6
xdustinface <xdustinfacex@gmail.com>
2022-02-22 12:27:32 +0100

    test: Move streamable tests to `tests.streamable`

--- tests/streamable/test_streamable.py
+++ tests/streamable/test_streamable.py
@@ -1,31 +1,31 @@
+import io
 import unittest
 from dataclasses import dataclass
 from typing import List, Optional, Tuple
-import io
 
 from clvm_tools import binutils
 from pytest import raises
 
 from chia.protocols.wallet_protocol import RespondRemovals
+from chia.streamable import (
+    Streamable,
+    parse_bool,
+    parse_bytes,
+    parse_list,
+    parse_optional,
+    parse_size_hints,
+    parse_str,
+    parse_tuple,
+    parse_uint32,
+    streamable,
+    write_uint32,
+)
 from chia.types.blockchain_format.coin import Coin
 from chia.types.blockchain_format.program import Program
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.full_block import FullBlock
 from chia.types.weight_proof import SubEpochChallengeSegment
 from chia.util.ints import uint8, uint32
-from chia.streamable import (
-    Streamable,
-    streamable,
-    parse_bool,
-    parse_uint32,
-    write_uint32,
-    parse_optional,
-    parse_bytes,
-    parse_list,
-    parse_tuple,
-    parse_size_hints,
-    parse_str,
-)
 from tests.setup_nodes import bt, test_constants
 
 
